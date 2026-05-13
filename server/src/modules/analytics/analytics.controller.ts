41dd7e9296a5dfeb142414dd437c41b7601470f5
Saurabh Ravte <saurabh.ravte@gmail.com>
2026-05-13 21:57:47 +0530

    server : added analytical controller endpoint

--- server/src/modules/analytics/analytics.controller.ts
+++ server/src/modules/analytics/analytics.controller.ts
@@ -4,7 +4,6 @@ import ApiResponse from "../../common/utils/api-response";
 import PollModel from "../polls/polls.model";
 import ApiError from "../../common/utils/api-error";
 
-/** Owner-only analytics  */
 const getForCreator = async (
   req: Request,
   res: Response,
@@ -16,14 +15,15 @@ const getForCreator = async (
     if (!poll) throw ApiError.notFound("Poll not found");
     if (poll.creatorId !== req.user!.id) throw ApiError.forbidden();
 
-    const data = await analyticsService.getAnalytics(poll.id);
+    const data = await analyticsService.getAnalytics(poll.id, {
+      includeRespondents: true,
+    });
     return ApiResponse.ok(res, "Analytics", data);
   } catch (err) {
     next(err);
   }
 };
 
-/** Public analytics */
 const getPublic = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const slug = String(req.params.slug);
@@ -32,7 +32,9 @@ const getPublic = async (req: Request, res: Response, next: NextFunction) => {
     if (!poll.resultsPublished) {
       throw ApiError.forbidden("Results have not been published yet");
     }
-    const data = await analyticsService.getAnalytics(poll.id);
+    const data = await analyticsService.getAnalytics(poll.id, {
+      includeRespondents: false,
+    });
     return ApiResponse.ok(res, "Analytics", data);
   } catch (err) {
     next(err);
